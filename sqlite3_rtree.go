5a3ff9cd5fd8ede4390ab8110348760e2ae7314f
Keishi Hattori <keishi.hattori@gmail.com>
2016-04-15 20:47:01 +0900

    Add rtree build tag

---
+++ sqlite3_rtree.go
@@ -0,0 +1,12 @@
+// Copyright (C) 2014 Yasuhiro Matsumoto <mattn.jp@gmail.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+// +build rtree
+
+package sqlite3
+
+/*
+#cgo CFLAGS: -DSQLITE_ENABLE_RTREE
+*/
+import "C"
